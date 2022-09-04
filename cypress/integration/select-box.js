afff2415d1f6b9db54a02d6199ff2e54e6662cb6
zarkonov <zarko.novicic@gmail.com>
2022-09-04 20:23:17 +0200

    final project's  commit

--- cypress/integration/select-box.js
+++ cypress/integration/select-box.js
@@ -1,7 +1,7 @@
 describe('Select box', () => {
     it('should pick an option from select box', () => {
         cy.visit('https://devexpress.github.io/testcafe/example/')
-        cy.get('#preffered-interface'). select('JavaScript API')//select JavaScript API option from drop down menu
+        cy.get('#preferred-interface'). select('JavaScript API')//select JavaScript API option from drop down menu
         cy.get('#preffered-interface').should('have.value', 'JavaScript API' )//checks if the choice from dropdown menu is the one we wanted
         cy.get('#submit-button').click()
         cy.get('#article-header').contains('Thank you')//grabs title from h1 when inspected
